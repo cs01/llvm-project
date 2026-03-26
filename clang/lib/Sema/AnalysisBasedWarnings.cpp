4c978e759d8ff36e9ea45fb1ad26581a946e895b
Chad Smith <cssmith@fb.com>
2026-03-26 12:02:11 -0700

    add comprehensive test suite and performance benchmark for flow-nullability

--- clang/lib/Sema/AnalysisBasedWarnings.cpp
+++ clang/lib/Sema/AnalysisBasedWarnings.cpp
@@ -3174,6 +3174,7 @@ void clang::sema::AnalysisBasedWarnings::IssueWarnings(
                         D->getBeginLoc())) {
     if (shouldEnableFlowNullability(S, D)) {
       if (AC.getCFG()) {
+        llvm::TimeTraceScope TimeProfile("FlowNullabilityAnalysis");
         FlowNullabilityReporter Reporter(S);
         NullabilityKind Default = S.getLangOpts().getNullabilityDefault();
         bool StrictMode = (Default != NullabilityKind::Unspecified);
