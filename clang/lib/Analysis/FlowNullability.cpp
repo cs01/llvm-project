90ec2f4b30aeeecf75e5bd020981eb3c4a431e9f
Chad Smith <cssmith@fb.com>
2026-03-27 16:49:55 -0700

    fix clang-format issues in header banner and alignment

--- clang/lib/Analysis/FlowNullability.cpp
+++ clang/lib/Analysis/FlowNullability.cpp
@@ -1,4 +1,4 @@
-//===- FlowNullability.cpp - Flow-sensitive null dereference checking ------===//
+//===- FlowNullability.cpp - Flow-sensitive null dereference checking -----===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
